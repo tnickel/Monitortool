717120ef0a6b1d815e1c084578cd7efc96a7b1f9
tnickel2019 <analyser2008@googlemail.com>
2020-08-17 22:14:09 +0200

    add new files for eclipse

---
+++ Test/src/v1/NewComposite.java
@@ -0,0 +1,81 @@
+package v1;
+
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.SWT;
+
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class NewComposite extends org.eclipse.swt.widgets.Composite {
+
+	/**
+	* Auto-generated main method to display this 
+	* org.eclipse.swt.widgets.Composite inside a new Shell.
+	*/
+	public static void main(String[] args) {
+		showGUI();
+	}
+	
+	/**
+	* Overriding checkSubclass allows this class to extend org.eclipse.swt.widgets.Composite
+	*/	
+	protected void checkSubclass() {
+	}
+	
+	/**
+	* Auto-generated method to display this 
+	* org.eclipse.swt.widgets.Composite inside a new Shell.
+	*/
+	public static void showGUI() {
+		Display display = Display.getDefault();
+		Shell shell = new Shell(display);
+		NewComposite inst = new NewComposite(shell, SWT.NULL);
+		Point size = inst.getSize();
+		shell.setLayout(new FillLayout());
+		shell.layout();
+		if(size.x == 0 && size.y == 0) {
+			inst.pack();
+			shell.pack();
+		} else {
+			Rectangle shellBounds = shell.computeTrim(0, 0, size.x, size.y);
+			shell.setSize(shellBounds.width, shellBounds.height);
+		}
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+	}
+
+	public NewComposite(org.eclipse.swt.widgets.Composite parent, int style) {
+		super(parent, style);
+		initGUI();
+	}
+
+	private void initGUI() {
+		try {
+			FormLayout thisLayout = new FormLayout();
+			this.setLayout(thisLayout);
+			this.setSize(374, 313);
+			this.layout();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ workspace_monitortool2019/test2/src/composite/NewComposite.java
@@ -0,0 +1,67 @@
+package composite;
+
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.SWT;
+
+public class NewComposite extends org.eclipse.swt.widgets.Composite {
+
+	/**
+	* Auto-generated main method to display this 
+	* org.eclipse.swt.widgets.Composite inside a new Shell.
+	*/
+	public static void main(String[] args) {
+		showGUI();
+	}
+	
+	/**
+	* Overriding checkSubclass allows this class to extend org.eclipse.swt.widgets.Composite
+	*/	
+	protected void checkSubclass() {
+	}
+	
+	/**
+	* Auto-generated method to display this 
+	* org.eclipse.swt.widgets.Composite inside a new Shell.
+	*/
+	public static void showGUI() {
+		Display display = Display.getDefault();
+		Shell shell = new Shell(display);
+		NewComposite inst = new NewComposite(shell, SWT.NULL);
+		Point size = inst.getSize();
+		shell.setLayout(new FillLayout());
+		shell.layout();
+		if(size.x == 0 && size.y == 0) {
+			inst.pack();
+			shell.pack();
+		} else {
+			Rectangle shellBounds = shell.computeTrim(0, 0, size.x, size.y);
+			shell.setSize(shellBounds.width, shellBounds.height);
+		}
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+	}
+
+	public NewComposite(org.eclipse.swt.widgets.Composite parent, int style) {
+		super(parent, style);
+		initGUI();
+	}
+
+	private void initGUI() {
+		try {
+			FormLayout thisLayout = new FormLayout();
+			this.setLayout(thisLayout);
+			this.layout();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
