19748212199041cb10ec75543c4d50c67643fd6a
tnickel2019 <analyser2008@googlemail.com>
2021-01-30 21:48:18 +0100

    monitortool: add powershell skript to distribute eas

---
+++ skripts/getnewEas/getNewEas.ps1
@@ -0,0 +1,18 @@
+﻿$monroot="D:\Forex\StrategienImEinsatz"
+$srcroot="C:\Users\Thomas Nickel\Google Drive\United Generation 2019\InternalResourcesNotForPublic\Thomas\EAs"
+
+
+$src="Weekly EAs for Real\MT4+EAs+Week1+Feb+2021"  
+$dest="FxOpen2_FSB_weekly_for_real"
+Remove-Item  "$monroot\$dest\*" 
+Copy-Item -Path "$srcroot\$src\*" "$monroot\$dest" 
+
+$src="Weekly EAs for Real\MT4+EAs+Week1+Feb+2021"  
+$dest="JFD2_FSB_weekly_for_real"
+Remove-Item  "$monroot\$dest\*" 
+Copy-Item -Path "$srcroot\$src\*" "$monroot\$dest" 
+
+$src="Weekly EAs for Real\MT4+EAs+Week1+Feb+2021"  
+$dest="tickmill2_FSB_weekly_for_real"
+Remove-Item  "$monroot\$dest\*" 
+Copy-Item -Path "$srcroot\$src\*" "$monroot\$dest" 
