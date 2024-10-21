7bf807c31b88ab6751f4a338cd44290f6026de67
tnickel2019 <analyser2008@googlemail.com>
2024-10-21 15:51:23 +0200

    Metrikanlyzer: update grapher class

--- Libs/src/gui/WekaAttributeGrapher.java
+++ Libs/src/gui/WekaAttributeGrapher.java
@@ -7,20 +7,27 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.statistics.HistogramDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import java.awt.*;
-import java.io.BufferedReader;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 
 public class WekaAttributeGrapher extends ApplicationFrame {
 
@@ -29,20 +36,29 @@ public class WekaAttributeGrapher extends ApplicationFrame {
     public WekaAttributeGrapher(String title, String rootPath) {
         super(title);
         JTabbedPane tabbedPane = new JTabbedPane();
+tabbedPane.setFont(new java.awt.Font("Dialog", java.awt.Font.BOLD, 26));
 
-        // Alle unterverzeichnisse durchsuchen und CSV-Dateien finden
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        JPanel chartsContainer = new JPanel();
+        chartsContainer.setLayout(new BoxLayout(chartsContainer, BoxLayout.Y_AXIS));
+
+        JScrollPane chartsScrollPane = new JScrollPane(chartsContainer);
+chartsScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+chartsScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        chartsScrollPane.getVerticalScrollBar().setUnitIncrement(16);
+
+        // Alle Unterverzeichnisse durchsuchen und CSV-Dateien finden
         List<File> csvFiles = findCSVFiles(new File(rootPath));
 
+        Map<String, Map<String, JFreeChart>> attributeChartsMap = new LinkedHashMap<String, Map<String, JFreeChart>>();
         for (File csvFile : csvFiles) {
-            JPanel chartPanel = new JPanel();
-            chartPanel.setLayout(new BoxLayout(chartPanel, BoxLayout.Y_AXIS));
-
             List<String[]> data = readCSV(csvFile.getAbsolutePath());
             if (data != null && data.size() > 1) {
                 // Die erste Zeile enthält die Attributnamen
                 String[] headers = data.get(0);
+                String directoryName = csvFile.getParentFile().getName();
 
-                // Liste für positive und negative Werte
+                // Listen für positive und negative Werte initialisieren
                 List<List<Double>> positiveValues = new ArrayList<List<Double>>();
                 List<List<Double>> negativeValues = new ArrayList<List<Double>>();
 
@@ -55,20 +71,28 @@ public class WekaAttributeGrapher extends ApplicationFrame {
                 // Verarbeiten der Datenzeilen
                 for (int i = 1; i < data.size(); i++) {
                     String[] row = data.get(i);
-                    double wekaEndtestValue = Double.parseDouble(row[headers.length - 1]);
+                    if (row.length < headers.length) {
+                        // Überspringen von Zeilen mit unzureichenden Spalten
+                        continue;
+                    }
+                    try {
+                        double wekaEndtestValue = Double.parseDouble(row[headers.length - 1]);
 
-                    for (int j = 1; j < headers.length - 1; j++) {
-                        try {
-                            double attributeValue = Double.parseDouble(row[j]);
+                        for (int j = 1; j < headers.length - 1; j++) {
+                            try {
+                                double attributeValue = Double.parseDouble(row[j]);
 
-                            if (wekaEndtestValue >= 0) {
-                                positiveValues.get(j - 1).add(attributeValue);
-                            } else {
-                                negativeValues.get(j - 1).add(attributeValue);
+                                if (wekaEndtestValue >= 0) {
+                                    positiveValues.get(j - 1).add(attributeValue);
+                                } else {
+                                    negativeValues.get(j - 1).add(attributeValue);
+                                }
+                            } catch (NumberFormatException e) {
+                                // Überspringen von nicht-numerischen Attributen
                             }
-                        } catch (NumberFormatException e) {
-                            // Überspringen von nicht-numerischen Attributen
                         }
+                    } catch (NumberFormatException e) {
+                        // Überspringen der gesamten Zeile, wenn der Endtest-Wert nicht numerisch ist
                     }
                 }
 
@@ -78,37 +102,42 @@ public class WekaAttributeGrapher extends ApplicationFrame {
 
                     // Erstellen des Histogramms für positive und negative Werte
                     JFreeChart chart = createHistogramWithLines(attributeName, positiveValues.get(i - 1), negativeValues.get(i - 1));
-                    ChartPanel chartPanelForAttribute = new ChartPanel(chart);
-                    chartPanelForAttribute.setPreferredSize(new Dimension(600, 300)); // Kleinere Diagrammgröße für bessere Darstellung
-                    chartPanel.add(chartPanelForAttribute);
+
+                    if (!attributeChartsMap.containsKey(attributeName)) {
+                        attributeChartsMap.put(attributeName, new LinkedHashMap<String, JFreeChart>());
+                    }
+                    attributeChartsMap.get(attributeName).put(directoryName, chart);
                 }
+            }
+        }
+
+        // Erstellen der Tabs, wobei jedes Attribut in einem eigenen Scrollbereich dargestellt wird
+        for (Map.Entry<String, Map<String, JFreeChart>> entry : attributeChartsMap.entrySet()) {
+            String attributeName = entry.getKey();
+            Map<String, JFreeChart> charts = entry.getValue();
+
+            JPanel chartPanel = new JPanel();
+            chartPanel.setLayout(new BoxLayout(chartPanel, BoxLayout.Y_AXIS));
+            for (Map.Entry<String, JFreeChart> chartEntry : charts.entrySet()) {
+                String directoryName = chartEntry.getKey();
+                JFreeChart chart = chartEntry.getValue();
+
+                ChartPanel chartPanelForAttribute = new ChartPanel(chart);
+                chartPanelForAttribute.setPreferredSize(new Dimension(800, 400)); // Kleinere Diagrammgröße für bessere Darstellung
+                chartPanelForAttribute.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK), directoryName, 0, 0, new java.awt.Font("Dialog", java.awt.Font.BOLD, 24)));
+                chartPanel.add(chartPanelForAttribute);
             }
 
             JScrollPane scrollPane = new JScrollPane(chartPanel);
+scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
             scrollPane.getVerticalScrollBar().setUnitIncrement(16);
-            String directoryName = csvFile.getParentFile().getName();
-            String tabName = directoryName.substring(directoryName.indexOf('_') + 1);
-            tabbedPane.addTab(tabName, scrollPane);
+            tabbedPane.addTab(attributeName, scrollPane);
         }
 
-        tabbedPane.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                JTabbedPane sourceTabbedPane = (JTabbedPane) e.getSource();
-                JScrollPane selectedScrollPane = (JScrollPane) sourceTabbedPane.getSelectedComponent();
-                if (selectedScrollPane != null) {
-                    selectedScrollPane.getVerticalScrollBar().setValue(lastScrollPosition);
-                    selectedScrollPane.getVerticalScrollBar().addAdjustmentListener(new java.awt.event.AdjustmentListener() {
-                        @Override
-                        public void adjustmentValueChanged(java.awt.event.AdjustmentEvent adjustmentEvent) {
-                            lastScrollPosition = adjustmentEvent.getValue();
-                        }
-                    });
-                }
-            }
-        });
-
-        setContentPane(tabbedPane);
+        mainPanel.add(tabbedPane, BorderLayout.CENTER);
+        chartsScrollPane.getViewport().setViewPosition(new java.awt.Point(0, 0));
+        setContentPane(mainPanel);
     }
 
     // Methode zum Erstellen eines Histogramms mit Linien
@@ -116,17 +145,11 @@ public class WekaAttributeGrapher extends ApplicationFrame {
         HistogramDataset dataset = new HistogramDataset();
 
         // Hinzufügen der positiven Werte
-        double[] positiveArray = new double[positiveValues.size()];
-        for (int i = 0; i < positiveValues.size(); i++) {
-            positiveArray[i] = positiveValues.get(i);
-        }
+        double[] positiveArray = listToPrimitiveArray(positiveValues);
         dataset.addSeries("Positive Values", positiveArray, 20);
 
         // Hinzufügen der negativen Werte
-        double[] negativeArray = new double[negativeValues.size()];
-        for (int i = 0; i < negativeValues.size(); i++) {
-            negativeArray[i] = negativeValues.get(i);
-        }
+        double[] negativeArray = listToPrimitiveArray(negativeValues);
         dataset.addSeries("Negative Values", negativeArray, 20);
 
         JFreeChart chart = ChartFactory.createHistogram(
@@ -154,32 +177,50 @@ public class WekaAttributeGrapher extends ApplicationFrame {
         return chart;
     }
 
-    // Methode zum Lesen der CSV-Datei
+    // Hilfsmethode zum Konvertieren von List<Double> zu double[]
+    private double[] listToPrimitiveArray(List<Double> list) {
+        double[] array = new double[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
+    }
+
+    // Verbesserte Methode zum Lesen der CSV-Datei mit OpenCSV
     private List<String[]> readCSV(String filePath) {
         List<String[]> data = new ArrayList<String[]>();
-
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                data.add(values);
-            }
+        CSVReader reader = null;
+        try {
+            reader = new CSVReader(new FileReader(filePath));
+            data = reader.readAll();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (CsvException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
         return data;
     }
 
     // Methode zum Finden aller CSV-Dateien im Verzeichnis
     private List<File> findCSVFiles(File root) {
-        List<File> csvFiles = new ArrayList<>();
+        List<File> csvFiles = new ArrayList<File>();
         if (root.isDirectory()) {
-            for (File file : root.listFiles()) {
-                if (file.isDirectory()) {
-                    csvFiles.addAll(findCSVFiles(file));
-                } else if (file.getName().equals("exported_for_weka.csv")) {
-                    csvFiles.add(file);
+            File[] files = root.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        csvFiles.addAll(findCSVFiles(file));
+                    } else if (file.getName().equalsIgnoreCase("exported_for_weka.csv")) {
+                        csvFiles.add(file);
+                    }
                 }
             }
         }
@@ -194,7 +235,7 @@ public class WekaAttributeGrapher extends ApplicationFrame {
     public static void displayGraphs(String rootPath) {
         WekaAttributeGrapher grapher = new WekaAttributeGrapher("Weka Attribute Grapher", rootPath);
         grapher.pack();
-        grapher.setSize(grapher.getWidth(), grapher.getHeight() - 5);
+        grapher.setSize(1200, 800); // Setzen Sie eine feste Größe für bessere Konsistenz
         grapher.setVisible(true);
         grapher.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
